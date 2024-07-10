b2bb156f8cd250cff483c3502df200c5ec77ceed
Ishan Gurjar <ishangurjar144@gmail.com>
2024-07-10 11:56:27 +0530

    project added

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='h-screen w-screen text-3xl flex justify-center items-center bg-slate-900 text-zinc-100 '>
+      Food App 
+    </div>
+  )
+}
+
+export default App
